1c9e636a6f415d00b6fde896bc7581287cc3fb01
aimandzhi <aigulmndzh@gmail.com>
2024-09-21 21:27:21 +0300

    edit main

---
+++ cmd/app/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/dugtriol/BarterApp/internal/app"
+
+const configPath = "config/config.yaml"
+
+func main() {
+	app.Run(configPath)
+
+}
---
+++ internal/app/app.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"log"
+
+	"github.com/dugtriol/BarterApp/config"
+)
+
+func Run(configPath string) {
+	// Configuration
+	cfg, err := config.NewConfig(configPath)
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	// Logger
+	SetLogrus(cfg.Log.Level)
+
+}
