6f5cf1910914e9f72d48456f40c69c4606c4dda4
aimandzhi <aigulmndzh@gmail.com>
2024-09-22 12:33:31 +0300

    add new hash password functions

--- pkg/hasher/password.go
+++ pkg/hasher/password.go
@@ -1,25 +1,41 @@
 package hasher
 
 import (
-	"crypto/sha1"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-type PasswordHasher interface {
-	Hash(password string) string
+//type PasswordHasher interface {
+//	//Hash(password string) string
+//	HashPassword(password string) (string, error)
+//	CheckPassword(password string, hashedPassword string) error
+//}
+//
+//type SHA1Hasher struct {
+//	salt string
+//}
+//
+//func NewSHA1Hasher(salt string) *SHA1Hasher {
+//	return &SHA1Hasher{salt: salt}
+//}
+//
+//func (h *SHA1Hasher) Hash(password string) string {
+//	hash := sha1.New()
+//	hash.Write([]byte(password))
+//
+//	return fmt.Sprintf("%x", hash.Sum([]byte(h.salt)))
+//}
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	return string(hashedPassword), nil
 }
 
-type SHA1Hasher struct {
-	salt string
-}
-
-func NewSHA1Hasher(salt string) *SHA1Hasher {
-	return &SHA1Hasher{salt: salt}
-}
-
-func (h *SHA1Hasher) Hash(password string) string {
-	hash := sha1.New()
-	hash.Write([]byte(password))
-
-	return fmt.Sprintf("%x", hash.Sum([]byte(h.salt)))
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
