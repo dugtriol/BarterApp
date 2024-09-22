7fb67811249c9e29b93d8b9df4e93266a85471de
aimandzhi <aigulmndzh@gmail.com>
2024-09-22 12:32:47 +0300

    edit sql files

--- migrations/000001_init_tables.up.sql
+++ migrations/000001_init_tables.up.sql
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS users
 (
     id    UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name  VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     phone VARCHAR(50),
+    password VARCHAR(100) NOT NULL,
     city  VARCHAR(50),
     mode  user_mode
-
 );
 
 DROP TYPE IF EXISTS product_category CASCADE;
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS products
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name  VARCHAR(50) NOT NULL,
     description VARCHAR(50) NOT NULL,
-    image  VARCHAR(50),
+    image  VARCHAR(150),
     status product_status,
     category product_category,
     user_id UUID REFERENCES users (id) ON DELETE CASCADE,
