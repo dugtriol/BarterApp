159ecf71984bafaa61eb4d744e025e59aeb9f897
aimandzhi <aigulmndzh@gmail.com>
2024-09-22 22:37:49 +0300

    add error file

---
+++ internal/controller/errors.go
@@ -0,0 +1,12 @@
+package controller
+
+import "errors"
+
+var (
+	ErrNotFound         = errors.New("not found")
+	ErrNotAuthenticated = errors.New("not authenticated")
+	ErrNotCreated       = errors.New("not created")
+	ErrNotValid         = errors.New("failed to unmarshall")
+	ErrAlreadyExists    = errors.New("already exists")
+	ErrInvalidPassword  = errors.New("invalid password")
+)
