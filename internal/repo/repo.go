b82e0e524963287d5183a94844ac43099745f1db
aimandzhi <aigulmndzh@gmail.com>
2024-09-22 22:40:06 +0300

    add methods to interface

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -4,21 +4,29 @@ import (
 	"context"
 
 	"github.com/dugtriol/BarterApp/internal/entity"
+	"github.com/dugtriol/BarterApp/internal/repo/pgdb"
+	"github.com/dugtriol/BarterApp/pkg/postgres"
 )
 
 type User interface {
-	CreateUser(ctx context.Context, user entity.User) (int, error)
-	GetUserByUsernameAndPassword(ctx context.Context, username, password string) (entity.User, error)
-	GetUserById(ctx context.Context, id int) (entity.User, error)
-	GetUserByUsername(ctx context.Context, username string) (entity.User, error)
+	Create(ctx context.Context, user entity.User) (entity.User, error)
+	GetById(ctx context.Context, id string) (entity.User, error)
+	GetByUsername(ctx context.Context, username string) (entity.User, error)
+	GetByEmail(ctx context.Context, email string) (entity.User, error)
 }
 
+type Product interface {
+	Create(ctx context.Context, product entity.Product) (entity.Product, error)
+	GetById(ctx context.Context, id string) (entity.Product, error)
+}
 type Repositories struct {
 	User
+	Product
 }
 
-//func NewRepositories(pg *postgres.Postgres) *Repositories {
-//	return &Repositories{
-//		User:        pgdb.NewUserRepo(pg),
-//	}
-//}
+func NewRepositories(pg *postgres.Database) *Repositories {
+	return &Repositories{
+		User:    pgdb.NewUserRepo(pg),
+		Product: pgdb.NewProductRepo(pg),
+	}
+}
