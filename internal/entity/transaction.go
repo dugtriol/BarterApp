8f2d737e9c41b8eec0815c94e7f6ec16ca5dffb4
aimandzhi <aigulmndzh@gmail.com>
2024-09-26 11:43:58 +0300

    change time fields

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -1,14 +1,16 @@
 package entity
 
+import "time"
+
 type Transaction struct {
-	Id             string `yaml:"id"`
-	Owner          string `yaml:"owner"`
-	Buyer          string `yaml:"buyer"`
-	ProductIdBuyer string `yaml:"product_id_buyer"`
-	ProductIdOwner string `yaml:"product_id_owner"`
-	Status         string `yaml:"status"`
-	CreatedAt      string `yaml:"created_at"`
-	UpdatedAt      string `yaml:"updated_at"`
-	Shipping       string `yaml:"shipping"`
-	Address        string `yaml:"address"`
+	Id             string    `yaml:"id"`
+	Owner          string    `yaml:"owner"`
+	Buyer          string    `yaml:"buyer"`
+	ProductIdBuyer string    `yaml:"product_id_buyer"`
+	ProductIdOwner string    `yaml:"product_id_owner"`
+	Status         string    `yaml:"status"`
+	CreatedAt      time.Time `yaml:"created_at"`
+	UpdatedAt      time.Time `yaml:"updated_at"`
+	Shipping       string    `yaml:"shipping"`
+	Address        string    `yaml:"address"`
 }
